3261794ea91b23b71c70dbc7e78d0d64198cd834
NamrataPawar25 <namratapawar9793@gmail.com>
2025-08-07 13:14:22 +0530

    Dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import useBlog from '../hook/useBlog'
+import { Link } from 'react-router-dom'
+
+const Dashboard = () => {
+    const [blogs, setBlogs] = useState([])
+    const {getBlogs} = useBlog()
+
+    function fetchData(){
+        const getBlogsLocal = getBlogs()
+        setBlogs(getBlogsLocal)
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    function GetBlogByID(){
+      
+    }
+
+  return (
+    <div>
+      <h1 className='my-3'>DASHBOARD</h1>
+      {blogs.length >0 ?(
+        <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Blog Title</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {blogs.map((b,i)=>(
+      <tr key={i}>
+      <td>{i+1}</td>
+      <td><Link to={`/${b.id}`}>{b.title}</Link></td>
+      <td><button className='btn btn-success me-2'>Edit</button>
+        <button className='btn btn-danger'>Delete</button></td>
+    </tr>
+    ))}
+    
+  </tbody>
+</table>
+      ) : (<p>No blog yet</p>)}
+    </div>
+  )
+}
+
+export default Dashboard
