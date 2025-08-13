7d830552a6266b2e3eb85d0f54dd3c1e0c0b6cd0
NamrataPawar25 <namratapawar9793@gmail.com>
2025-08-13 12:07:59 +0530

    Blog App

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Dashboard from './pages/Dashboard'
+import BlogDetails from './pages/BlogDetails'
+import AddBlog from './pages/AddBlog'
+import UpdateBlog from './pages/UpdateBlog'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Dashboard />}></Route>
+          <Route path='/:ID' element={<BlogDetails />}></Route>
+          <Route path='/create' element={<AddBlog />}></Route>
+          <Route path='/update/:ID' element={<UpdateBlog />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
